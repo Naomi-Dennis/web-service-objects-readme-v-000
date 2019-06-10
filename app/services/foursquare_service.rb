7732c6d2e4ddc1e4f2ce4c3899604117c1884720
Naomi Dennis <njdennis94@gmail.com>
2019-06-10 19:30:57 +0000

    Automatically backed up by Learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,5 +1,5 @@
 class FourSquareService
-  def authenticate!(client_secret, client_id, redirect_uri)
+  def authenticate!(client_id, redirect_uri)
     foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
   end
 end 
